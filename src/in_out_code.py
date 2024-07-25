03e3815315efbf9dc763f37ee9e112ac1d21320a
GGN_2015 <premierbob@qq.com>
2024-07-26 02:28:06 +0800

    feat:pd_code_to_dt_code

---
+++ src/in_out_code.py
@@ -0,0 +1,22 @@
+# 桥接：https://github.com/TopologicalKnotIndexer/get_in_out_code
+import os
+DIRNOW = os.path.dirname(os.path.abspath(__file__))
+SUBDIR = os.path.join(DIRNOW, "get_in_out_code", "src") # 子包路径
+
+
+# ======================================== BEGIN IMPORT FROM PATH ======================================== #
+import importlib
+import json
+import sys
+def load_module_from_path(path: str, mod_name: str): # 从指定路径导入一个包
+    assert os.path.isdir(path)                       # 路径必须存在
+    path         = os.path.abspath(path)             # 获得绝对路径
+    old_sys_path = json.loads(json.dumps(sys.path))  # 存档旧的 sys.path
+    sys.path     = [path] + sys.path                 # 将新的路径加入 sys.path
+    mod          = importlib.import_module(mod_name) # 加载指定的包
+    sys.path     = old_sys_path                      # 恢复旧的 sys.path
+    return mod
+# ======================================== END IMPORT FROM PATH ======================================== #
+
+def in_out_code(pd_code: list) -> list:
+    return load_module_from_path(SUBDIR, "get_in_out_code").get_in_out_code(pd_code)
---
+++ src/pd_code_to_dt_code.py
@@ -0,0 +1,24 @@
+# 假设给出的 pd_code 是合法的
+# 试图计算与之对应同一个扭结的 dt_code
+
+from in_out_code import in_out_code
+
+def pd_code_to_dt_code(pd_code: list) -> tuple:
+    io_code = in_out_code(pd_code)
+    dic    = {}
+    for i in range(len(pd_code)): # 考虑每个交叉点的编号处，谁在下方谁在上方
+        down_node = pd_code[i][0]
+        up_node   = pd_code[i][1] if io_code[i][1] == "IN" else pd_code[i][3]
+        assert ((down_node - up_node)%2) != 0 # 保证奇偶性不同
+        if down_node % 2== 0:
+            dic[up_node]   = +down_node # 偶数从下面穿过，标记为正
+        else:
+            dic[down_node] = -up_node   # 偶数从上面跨过，标记为负
+    dt_code = []
+    for i in range(len(pd_code)):
+        val = i * 2 + 1
+        dt_code.append(dic[val])
+    return tuple(dt_code)
+
+if __name__ == "__main__":
+    print(pd_code_to_dt_code([[1, 5, 2, 4], [3, 1, 4, 6], [5, 3, 6, 2]]))

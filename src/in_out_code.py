1704c25d90253a4cb39d6d3c2ebf69096051d352
GGN_2015 <neko@jlulug.org>
2026-07-14 00:34:16 +0800

    Resolve bundled incidence code relatively

--- src/in_out_code.py
+++ src/in_out_code.py
@@ -1,6 +1,9 @@
 """Compatibility import for the bundled incidence-direction algorithm."""
 
-from get_in_out_code.src.get_in_out_code import get_in_out_code
+try:
+    from .get_in_out_code.src.get_in_out_code import get_in_out_code
+except ImportError:  # Direct execution from the src directory.
+    from get_in_out_code.src.get_in_out_code import get_in_out_code
 
 
 def in_out_code(pd_code: list[list[int]]) -> list[list[str]]:
